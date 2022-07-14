193c612b99480d254b1bd0f6a83e6b88d9332f7e
flowersayo <flowersayo0811@ewhain.net>
2022-07-15 01:59:18 +0900

    feat: 둘러보기 메인 페이지 구현 #3
    resolves #3

--- src/components/common/SmallBanner.js
+++ src/components/common/SmallBanner.js
@@ -6,32 +6,33 @@ import Icon from '../../assets/images/Icon';
 import {theme} from '../../styles/theme';
 
 const Container = styled.View`
-  width: 100%;
+  width: ${({width}) => (width - 40) / 2 - 10}px;
   height: 80px;
-  background-color: ${theme.colors.main};
+  background-color: ${theme.colors.banner_bg};
   border-radius: 10px;
   display: flex;
   flex-direction: row;
-  justify-content: space-evenly;
+  justify-content: space-around;
   align-items: center;
+  padding-left: 9px;
+  background-color: ${theme.colors.small_banner_bg};
 `;
 
 const H3 = styled.Text`
-  color: white;
-  font-size: 16px;
+  color: black;
+  font-size: 14px;
   font-weight: 700;
 `;
 
-export default function CommonBanner({text, onClick}) {
+export default function SmallBanner({text, onClick}) {
   const width = Dimensions.get('window').width;
   console.log(Icon);
   return (
     <Container width={width}>
-      <Image source={Icon.SproutIcon} style={{width: 43, height: 43}} />
       <H3>{text}</H3>
       <Image
         source={Icon.RightArrowIcon}
-        style={{width: 24, height: 24, marginLeft: 30}}
+        style={{width: 24, height: 24, tintColor: theme.colors.banner}}
       />
     </Container>
   );
--- src/screens/AroundScreen.js
+++ src/screens/AroundScreen.js
@@ -1,11 +1,44 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import CommonBanner from '../components/common/CommonBanner';
+import styled from 'styled-components';
+import SmallBanner from '../components/common/SmallBanner';
+import PhotoBanner from '../components/common/PhotoBanner';
 
+const Container = styled.View`
+  display: flex;
+  padding: 20px;
+  flex: 1;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+const H1 = styled.Text`
+  color: black;
+  font-size: 24px;
+  font-weight: 700;
+  align-self: flex-start;
+`;
+
+const BannerWrapper = styled.View`
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  justify-content: space-between;
+`;
 //둘러보기
 export default function AroundScreen() {
   return (
-    <View>
-      <Text>AroundScreen</Text>
-    </View>
+    <Container>
+      <H1>둘러보기</H1>
+      <CommonBanner text="오늘의 친환경 스팟 확인하기" />
+      <BannerWrapper>
+        <SmallBanner text="전기차 충전소" />
+        <SmallBanner text="자전거 여행" />
+      </BannerWrapper>
+      <PhotoBanner text="ZEJE의 추천 여행지" />
+      <PhotoBanner text="ZEJE의 추천 액티비티" />
+    </Container>
   );
 }
