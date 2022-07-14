be3ffb4d9b7d58af05838734216385b54e012412
flowersayo <flowersayo0811@ewhain.net>
2022-07-14 23:26:54 +0900

    fix : move src folder to root

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import TabNavigator from './android/app/src/navigation/TabNavigator';
+import TabNavigator from './src/navigation/TabNavigator';
 export default function App() {
   return (
     <NavigationContainer>
