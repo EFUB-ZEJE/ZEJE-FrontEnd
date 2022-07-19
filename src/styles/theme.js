1ffde35f42e91649a992b631a7c59b3e7033ded9
flowersayo <flowersayo0811@ewhain.net>
2022-07-20 00:41:44 +0900

    add : add theme management file

---
+++ src/styles/theme.js
@@ -0,0 +1,19 @@
+export const palette = {
+  black: '#000000',
+  orange: '#FFA317', // Zeje 메인 색상
+  orange50: '#FFF8ED', // 리뷰 뒷 배경 옅은 오렌지
+  orange100: '#FEEED5', // 방문한 포인트 스팟 뒷배경
+  orange400: '#FFBA50', // 방문한 포인트 스팟 연한 글씨
+  green100: '#E6F6ED', // 배너 뒷배경
+  green300: '#9ADAB6', // 배너 화살표
+  gray600: '#221E1A', // 서체 색상
+  gray350: '#8E8A83', // 포인트 스팟 주소목록
+};
+
+export const theme = {
+  colors: {
+    main: palette.orange,
+    font: palette.gray600,
+    black: palette.black,
+  },
+};
