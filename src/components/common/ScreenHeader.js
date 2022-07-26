ea05d24b42343c7e2eb19316e951a52a8a853abf
Haerin Lee <dazzlynnnn@gmail.com>
2022-07-26 22:28:41 +0900

    add: ScreenHeader에 canSearch props 추가

--- src/components/common/ScreenHeader.js
+++ src/components/common/ScreenHeader.js
@@ -1,63 +1,79 @@
 import React from 'react';
 import styled from 'styled-components/native';
-import SvgIcon from './SvgIcon';
-import {theme} from '../../styles/theme.js';
+import {palette, theme} from '../../styles/theme.js';
 import font from '../fonts/font.js';
-import SizedBox from './SizedBox';
 import {TouchableOpacity} from 'react-native';
+import {SvgIcon, SizedBox, SearchBar} from '../../components';
 
 const ScreenHeader = ({
   navigation: {goBack},
   screenTitle,
   canGoBack,
   rightIcon,
+  canSearch,
+  placeholder,
+  handleChange,
 }) => {
   return (
     <HeaderWrapper>
-      <TitleWrapper>
-        {canGoBack ? (
-          <>
-            <TouchableOpacity
-              onPress={() => {
-                goBack();
-              }}>
-              <SvgIcon name="GoBack" />
-            </TouchableOpacity>
-            <SizedBox width={16} />
-          </>
-        ) : (
-          <EmptyIcon />
-        )}
-        <font.title.Headline color={theme.colors.main}>
-          {screenTitle}
-        </font.title.Headline>
-      </TitleWrapper>
-      <TouchableOpacity
-        onPress={() => {
-          goBack();
-        }}>
-        {rightIcon ? <SvgIcon name={rightIcon} /> : <></>}
-      </TouchableOpacity>
+      <LineWrapper>
+        <TitleWrapper>
+          {canGoBack ? (
+            <>
+              <TouchableOpacity
+                onPress={() => {
+                  goBack();
+                }}>
+                <SvgIcon name="GoBack" />
+              </TouchableOpacity>
+              <SizedBox width={16} />
+            </>
+          ) : (
+            <></>
+          )}
+          <font.title.Headline color={theme.colors.main}>
+            {screenTitle}
+          </font.title.Headline>
+        </TitleWrapper>
+        {/* 임시 */}
+        <TouchableOpacity
+          onPress={() => {
+            goBack();
+          }}>
+          {rightIcon ? <SvgIcon name={rightIcon} /> : <></>}
+        </TouchableOpacity>
+      </LineWrapper>
+      {canSearch ? (
+        <SearchBar placeholder={placeholder} handleChange={handleChange} />
+      ) : (
+        <></>
+      )}
     </HeaderWrapper>
   );
 };
 
 export default ScreenHeader;
 
-// 아래 화면이랑 구분하기 위해서 임시로 gray
 const HeaderWrapper = styled.View`
+  flex-direction: column;
+  align-items: center;
+  background-color: white;
+  padding: 16px;
+  border-bottom-width: 1px;
+  border-color: ${palette.gray150};
+`;
+
+// 아래 화면이랑 구분하기 위해서 임시로 gray
+const LineWrapper = styled.View`
+  width: 100%;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  background-color: gray;
-  padding: 12px 16px;
+  background-color: white;
+  padding: 12px 0;
 `;
 
 const TitleWrapper = styled.View`
   flex-direction: row;
   align-items: center;
-`;
-
-const EmptyIcon = styled.View`
-  width: 20px;
 `;
--- src/screens/Around/ActivityMainScreen.js
+++ src/screens/Around/ActivityMainScreen.js
@@ -1,18 +1,24 @@
-import {Text} from 'react-native';
-import React from 'react';
-import {ScreenContainer, ScreenHeader} from '../../components';
+import React, { useState } from 'react';
+import { Text } from 'react-native';
+import {ScreenContainer, ScreenHeader, SearchBar} from '../../components';
 
 //둘러보기
 export default function ActivityMainScreen({navigation}) {
+  const [search, setSearch] = useState('');
+  const _handleChange = (text) => {setSearch(text);}
   return (
     <>
       <ScreenHeader
         navigation={navigation}
-        screenTitle="ZEJE의 추천 액티비티"
+        screenTitle="ZEJE의 추천 여행지"
         canGoBack={true}
+        canSearch={true}
+        placeholder="액티비티의 이름이나 주소를 검색해보세요"
+        handleChange={_handleChange}
       />
       <ScreenContainer>
-        <Text>ActivityMain</Text>
+        <Text>검색어:</Text>
+        <Text>{search}</Text>
       </ScreenContainer>
     </>
   );
