d7c03af69df7315b22ee81c1c94f5b899b157b2a
Haerin Lee <dazzlynnnn@gmail.com>
2022-07-27 20:07:53 +0900

    add: Home, Around 상세 스크린 추가 #6

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,18 +1,29 @@
 import AroundScreen from './AroundScreen';
 import HomeScreen from './HomeScreen';
 import MyListScreen from './MyListScreen';
-import MyPageScreen from './MyPageScreen';
 import RememberScreen from './RememberScreen';
+
+import MypageMainScreen from './Home/MypageMainScreen';
+import AlertMainScreen from './Home/AlertMainScreen';
 
 import ActivityMainScreen from './Around/ActivityMainScreen';
 import TourMainScreen from './Around/TourMainScreen';
+import BatteryMainScreen from './Around/BatteryMainScreen';
+import BikeMainScreen from './Around/BikeMainScreen';
+import SpotMainScreen from './Around/SpotMainScreen';
+import ShareInfoMainScreen from './Around/ShareInfoMainScreen';
 
 export {
   AroundScreen,
   HomeScreen,
   MyListScreen,
-  MyPageScreen,
   RememberScreen,
+  MypageMainScreen,
+  AlertMainScreen,
   ActivityMainScreen,
   TourMainScreen,
+  BatteryMainScreen,
+  BikeMainScreen,
+  SpotMainScreen,
+  ShareInfoMainScreen,
 };
