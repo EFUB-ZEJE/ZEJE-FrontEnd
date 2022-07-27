af5c59757076a43c745ea9e76399227a2c3ebd9a
SJ-Lee33 <77518932+SJ-Lee33@users.noreply.github.com>
2022-07-27 18:52:19 +0900

    chore: homescreen 파일 위치 이동

--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -1,8 +1,8 @@
 import {View} from 'native-base';
 import React from 'react';
-import Header from '../components/home/header/Header';
-import Home from '../components/home/Home';
-import {theme} from '../styles/theme';
+import Header from '../../components/home/header/Header';
+import Home from '../../components/home/Home';
+import {theme} from '../../styles/theme';
 
 export default function HomeScreen() {
   return (
