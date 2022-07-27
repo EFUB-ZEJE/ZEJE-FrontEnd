7acbb09c6dce666879e1b7810b8d92a8192afadf
Haerin Lee <dazzlynnnn@gmail.com>
2022-07-27 20:18:40 +0900

    fix: ScreenContainer 아래 여백 수정 #6

--- src/components/common/ScreenContainer.js
+++ src/components/common/ScreenContainer.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import styled from 'styled-components/native';
+import SizedBox from './SizedBox';
 
 const ScreenContainer = ({children}) => {
-  return <Container>{children}</Container>;
+  return (
+    <Container>
+      {children}
+      <SizedBox height={30} />
+    </Container>
+  );
 };
 
 export default ScreenContainer;
