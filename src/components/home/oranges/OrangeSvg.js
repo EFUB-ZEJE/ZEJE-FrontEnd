88e5638bef316019d6949d79e2f7664fe362ea7c
SJ-Lee33 <77518932+SJ-Lee33@users.noreply.github.com>
2022-07-31 05:50:31 +0900

    feat: 오렌지 svg파일 사이즈조정을 위한 설정

--- src/components/home/oranges/OrangeSvg.js
+++ src/components/home/oranges/OrangeSvg.js
@@ -1,23 +1,10 @@
 import React from 'react';
-import styled from 'styled-components/native';
-
 import * as Oranges from '../../../assets/images/oranges';
 
-const OrangeSvg = ({name}) => {
+const OrangeSvg = ({name, width, height}) => {
   const Comp = Oranges[name];
 
-  return (
-    <IconContainer>
-      <Comp />
-    </IconContainer>
-  );
+  return <Comp width={width} height={height} />;
 };
 
 export default OrangeSvg;
-
-const IconContainer = styled.View`
-  justify-content: center;
-  align-items: center;
-  height: 48px;
-  width: 48px;
-`;
