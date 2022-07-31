eb4ceee1790a2d94c39322bc90425427a9106291
SJ-Lee33 <77518932+SJ-Lee33@users.noreply.github.com>
2022-07-31 14:34:48 +0900

    feat: 오렌지 설명 모달

--- src/modal/modals/OrangeGoldModal.js
+++ src/modal/modals/OrangeGoldModal.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import {useOrangeRedModal} from '../recoil/useModals';
+import {useOrangeGoldModal} from '../recoil/useModals';
 import ModalSheet from '../../components/common/modal/ModalSheet';
 import {Column} from 'native-base';
-import {OrangeRed} from '../../assets/images/oranges';
+import {OrangeGold} from '../../assets/images/oranges';
 import {Body_long1, Subhead_long3} from '../../styles/font';
 import OrangeModalCloseButton from '../../components/home/oranges/OrangeModalCloseButton';
 
-const OrangeRedModal = () => {
-  const {isModalOpen, closeModal} = useOrangeRedModal();
+const OrangeGoldModal = () => {
+  const {isModalOpen, closeModal} = useOrangeGoldModal();
 
   return (
     <ModalSheet isModalOpen={isModalOpen} closeModal={closeModal}>
       <Column space={3} alignItems={'center'} w={'100%'}>
-        <OrangeRed width="100" height="100" />
-        <Subhead_long3>붉게 물든 빛 레드향</Subhead_long3>
+        <OrangeGold width="100" height="100" />
+        <Subhead_long3>여왕의 품격, 황금향</Subhead_long3>
         <Body_long1>
-          껍질과 과육에 붉은 빛이 도는 레드향은 신맛이 적고, 다른 만감류에 비해
-          껍질을 벗기기가 편하다는 특징이 있습니다.
+          황금향은 여왕과 같은 품위를 지녔다고 해서 이런 이름이 붙었다고 합니다.
+          알맹이가 통통하고 신맛이 적으며, 과즙이 풍부합니다.
         </Body_long1>
         <OrangeModalCloseButton onPress={closeModal} />
       </Column>
@@ -24,4 +24,4 @@ const OrangeRedModal = () => {
   );
 };
 
-export default OrangeRedModal;
+export default OrangeGoldModal;
--- src/modal/modals/OrangeMtModal.js
+++ src/modal/modals/OrangeMtModal.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import {useOrangeRedModal} from '../recoil/useModals';
+import {useOrangeMtModal} from '../recoil/useModals';
 import ModalSheet from '../../components/common/modal/ModalSheet';
 import {Column} from 'native-base';
-import {OrangeRed} from '../../assets/images/oranges';
+import {OrangeMt} from '../../assets/images/oranges';
 import {Body_long1, Subhead_long3} from '../../styles/font';
 import OrangeModalCloseButton from '../../components/home/oranges/OrangeModalCloseButton';
 
-const OrangeRedModal = () => {
-  const {isModalOpen, closeModal} = useOrangeRedModal();
+const OrangeMtModal = () => {
+  const {isModalOpen, closeModal} = useOrangeMtModal();
 
   return (
     <ModalSheet isModalOpen={isModalOpen} closeModal={closeModal}>
       <Column space={3} alignItems={'center'} w={'100%'}>
-        <OrangeRed width="100" height="100" />
-        <Subhead_long3>붉게 물든 빛 레드향</Subhead_long3>
+        <OrangeMt width="100" height="100" />
+        <Subhead_long3>한라산을 닮은 모양 한라봉</Subhead_long3>
         <Body_long1>
-          껍질과 과육에 붉은 빛이 도는 레드향은 신맛이 적고, 다른 만감류에 비해
-          껍질을 벗기기가 편하다는 특징이 있습니다.
+          꼭지가 튀어나온 모양이 꼭 한라산같은 한라봉은 과육이 부드럽고, 과즙이
+          풍부하며 당도가 매우 높은 것이 특징이랍니다.
         </Body_long1>
         <OrangeModalCloseButton onPress={closeModal} />
       </Column>
@@ -24,4 +24,4 @@ const OrangeRedModal = () => {
   );
 };
 
-export default OrangeRedModal;
+export default OrangeMtModal;
