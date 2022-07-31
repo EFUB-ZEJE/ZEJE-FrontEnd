591cebbefb44e9ffac95203917c57f324a20293a
SJ-Lee33 <77518932+SJ-Lee33@users.noreply.github.com>
2022-07-31 15:11:50 +0900

    feat: Home 나무 오렌지 걸음수 진행바

---
+++ src/components/home/oranges/OrangeSmallProgressBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {View} from 'react-native';
+import * as Progress from 'react-native-progress';
+import {palette, theme} from '../../../styles/theme';
+
+export function OrangeSmallProgressBar({walk, maxWalk}) {
+  return (
+    <View style={{marginTop: -5}}>
+      <Progress.Bar
+        progress={walk / maxWalk}
+        width={null}
+        height={2}
+        borderColor={'white'}
+        borderWidth={2}
+        color={theme.colors.main}
+        unfilledColor={palette.gray150}
+      />
+    </View>
+  );
+}
+
+export default OrangeSmallProgressBar;
