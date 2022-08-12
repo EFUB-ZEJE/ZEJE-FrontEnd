1333c81c80faba7def7e3021f1e0529458cf3249
JangAyeon <jangayeon@JangAyeonui-MacBookPro.local>
2022-08-13 04:10:43 +0900

    #15 feat: bottomSheet keyboardControl & photoSelect

--- src/components/Remember/BottomSheet/FolderTitle.js
+++ src/components/Remember/BottomSheet/FolderTitle.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {KeyboardAvoidingView} from 'react-native';
+import {View} from 'react-native';
 import styled from 'styled-components/native';
 import SizedBox from '../../common/SizedBox';
 import SvgIcon from '../../common/SvgIcon';
 import BottomSheet from '../../common/BottomSheet';
 import {theme} from '../../../styles/theme';
 import font from '../../../styles/font';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
 export default function FolderTitle({
   modalVisible,
@@ -22,28 +23,33 @@ export default function FolderTitle({
     setModalVisible(false);
   };
   return (
-    <KeyboardAvoidingView>
-      <BottomSheet
-        modalVisible={modalVisible}
-        setModalVisible={setModalVisible}>
-        <HeaderContainer onPress={handleIconPress}>
-          <font.title.Subhead3 color={theme.colors.main}>
-            일기장 추가
-          </font.title.Subhead3>
-          <SvgIcon name="Close" size={'36px'} />
-        </HeaderContainer>
-        <SizedBox height={16} />
-        <FolderInput
-          placeholder="일기장 이름"
-          returnKeyType="done"
-          multiline={false}
-          onChangeText={folder => setFolder(folder)}
-          value={folder}
-          onSubmitEditing={handleOnSubmitEditing}
-        />
-        <SizedBox height={16} />
-      </BottomSheet>
-    </KeyboardAvoidingView>
+    <BottomSheet modalVisible={modalVisible} setModalVisible={setModalVisible}>
+      <HeaderContainer onPress={handleIconPress}>
+        <font.title.Subhead3 color={theme.colors.main}>
+          일기장 추가
+        </font.title.Subhead3>
+        <SvgIcon name="Close" size={'36px'} />
+      </HeaderContainer>
+      <SizedBox height={16} />
+
+      <View>
+        <KeyboardAwareScrollView
+          enableOnAndroid
+          extraScrollHeight={3}
+          extraHeight={3}>
+          <FolderInput
+            placeholder="일기장 이름"
+            returnKeyType="done"
+            multiline={false}
+            onChangeText={folder => setFolder(folder)}
+            value={folder}
+            onSubmitEditing={handleOnSubmitEditing}
+          />
+        </KeyboardAwareScrollView>
+      </View>
+
+      <SizedBox height={16} />
+    </BottomSheet>
   );
 }
 
@@ -51,8 +57,6 @@ const FolderInput = styled.TextInput`
   background-color: ${theme.colors.search};
   border-radius: 10px;
 `;
-
-const Container = styled.KeyboardAvoidingView``;
 
 const HeaderContainer = styled.TouchableOpacity`
   display: flex;
