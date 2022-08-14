28c8109718773099ef39470fd765178c79978ba7
SJ-Lee33 <77518932+SJ-Lee33@users.noreply.github.com>
2022-08-14 15:27:25 +0900

    feat: 걸음 수 세기 기능

--- src/navigation/StackNavigator.js
+++ src/navigation/StackNavigator.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import TabNavigator from './TabNavigator';
 import {
   HomeScreen,
@@ -17,10 +17,62 @@ import {
   DairyDetailScreen,
 } from '../screens';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {usePedometer} from '../feature/pedometer/recoil/usePedometer';
+import {
+  accelerometer,
+  SensorTypes,
+  setUpdateIntervalForType,
+} from 'react-native-sensors';
+import {saveData, STEP_COUNT} from '../services/LocalStorage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Stack = createNativeStackNavigator();
+setUpdateIntervalForType(SensorTypes.accelerometer, 400);
 
 export default function StackNavigator() {
+  const [xAcceleration, setXAcceleration] = useState(0);
+  const [yAcceleration, setYAcceleration] = useState(0);
+  const [zAcceleration, setZAcceleration] = useState(0);
+  const [magnitudePrevious, setMagnitudePrevious] = useState(0);
+
+  const {stepCount, setStepCount} = usePedometer();
+
+  async function initStepCount() {
+    const value = await AsyncStorage.getItem(STEP_COUNT);
+    setStepCount(parseInt(value));
+  }
+
+  useEffect(() => {
+    initStepCount();
+
+    const subscription = accelerometer
+      .pipe(data => data)
+      .subscribe(speed => {
+        setXAcceleration(speed.x);
+        setYAcceleration(speed.y);
+        setZAcceleration(speed.z);
+      });
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, []);
+
+  useEffect(() => {
+    const magnitude = Math.sqrt(
+      Math.pow(xAcceleration, 2) +
+        Math.pow(yAcceleration, 2) +
+        Math.pow(zAcceleration, 2),
+    );
+
+    const magnitudeDelta = magnitude - magnitudePrevious;
+    setMagnitudePrevious(() => magnitude);
+
+    if (magnitudeDelta > 2) {
+      setStepCount(prevSteps => prevSteps + 1);
+      saveData(STEP_COUNT, stepCount.toString());
+    }
+  }, [xAcceleration, yAcceleration, zAcceleration]);
+
   return (
     <Stack.Navigator
       initialRouteName="TabNavigator"
