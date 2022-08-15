56128e657c050df88ed1cfbb28ff2d26091d6580
flowersayo <flowersayo0811@ewhain.net>
2022-08-16 01:31:41 +0900

    add: create Axios instance & EV services
    전기차 충전소 조회용 Axios instance생성
    
    resolves #29

---
+++ src/utils/api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import {EV_STATION_BASE_URL} from '../constants/config';
+import Config from 'react-native-config';
+
+// 전기차 충전소 조회용 Axios
+export const EV_STATION_API = axios.create({
+  baseURL: EV_STATION_BASE_URL,
+  params: {
+    serviceKey: Config.EV_STATION_API_KEY,
+    pageNo: 1,
+    numOfRows: 10,
+    zcode: 50,
+    dataType: JSON,
+  },
+  withCredentials: false,
+  crossDomain: true,
+});
