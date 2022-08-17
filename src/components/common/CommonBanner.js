ac170de0b2aa55088eef94a431a02ee9ad875865
flowersayo <flowersayo0811@ewhain.net>
2022-08-18 05:14:53 +0900

    refactor: Common Banner generalization
    마이리스트에서 CommonBanner 컴포넌트 재활용을 위한 props 추가

--- src/components/common/CommonBanner.js
+++ src/components/common/CommonBanner.js
@@ -3,9 +3,21 @@ import styled from 'styled-components';
 import font from '../../styles/font';
 import SvgIcon from './SvgIcon';
 
-export default function CommonBanner({text, color, bgColor, navigation, path}) {
+export default function CommonBanner({
+  text,
+  color,
+  bgColor,
+  onPress,
+  border,
+
+  borderColor,
+}) {
   return (
-    <Container bgColor={bgColor} onPress={() => navigation.navigate(path)}>
+    <Container
+      bgColor={bgColor}
+      onPress={onPress}
+      border={border}
+      borderColor={borderColor}>
       <font.title.Subhead3 color={color}>{text}</font.title.Subhead3>
       <SvgIcon name="RightArrow" color={color} />
     </Container>
@@ -15,11 +27,13 @@ export default function CommonBanner({text, color, bgColor, navigation, path}) {
 const Container = styled.TouchableOpacity`
   width: 100%;
   height: 56px;
-  background-color: ${({bgColor}) => bgColor};
+  background-color: ${({bgColor}) => (bgColor ? bgColor : 'white')};
   border-radius: 10px;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   padding: 16px 20px;
   align-items: center;
+  border: ${({border}) => (border ? '1px solid' : 'none')};
+  border-color: ${({borderColor}) => (borderColor ? borderColor : 'white')};
 `;
