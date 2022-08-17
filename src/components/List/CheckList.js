5f08b612704d5bdb8e878715dd8d892f190499c5
flowersayo <flowersayo0811@ewhain.net>
2022-08-18 06:56:58 +0900

    feat: toggle task (completed/uncompleted)
    완료/비완료 토글
    
    resolves #33

--- src/components/List/CheckList.js
+++ src/components/List/CheckList.js
@@ -3,14 +3,12 @@ import React, {useState} from 'react';
 import font from '../../styles/font';
 import List from './List';
 import ListInput from './ListInput';
+
 export default function CheckList() {
-  // task 의 id는 그때 시간으로해서 겹치지(unique) 않도록
   const [tasks, setTasks] = useState({
     1: {id: 1, text: '잠옷바지', completed: false},
     2: {id: 2, text: '물티슈', completed: false},
     3: {id: 3, text: '양말', completed: false},
-
-    //randomNumber : {}
   });
 
   const _addTask = text => {
@@ -22,13 +20,25 @@ export default function CheckList() {
     setTasks({...tasks, ...newTaskObject});
   };
 
+  const _toggleTask = id => {
+    const currentTask = Object.assign({}, tasks);
+    currentTask[id]['completed'] = !currentTask[id]['completed'];
+    setTasks(currentTask);
+  };
+
   return (
     <View>
       <ListInput _addTask={_addTask} />
       {Object.values(tasks)
         .reverse()
         .map(task => (
-          <List key={task.id} text={task.text} completed={task.completed} />
+          <List
+            key={task.id}
+            id={task.id}
+            text={task.text}
+            completed={task.completed}
+            _toggleTask={_toggleTask}
+          />
         ))}
     </View>
   );
