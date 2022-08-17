30e96bf2f3c7046665ceda4fd339d67c3467cf63
flowersayo <flowersayo0811@ewhain.net>
2022-08-18 06:56:58 +0900

    feat: toggle task (completed/uncompleted)
    완료/비완료 토글
    
    resolves #33

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,19 +1,21 @@
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 import React from 'react';
 import SvgIcon from '../common/SvgIcon';
 import styled from 'styled-components';
 import font from '../../styles/font';
 import {theme} from '../../styles/theme';
 
-export default function List({text, completed}) {
+export default function List({id, text, completed, _toggleTask}) {
   return (
     <Container>
       <font.title.Subhead3>{text}</font.title.Subhead3>
-      {completed ? (
-        <SvgIcon name="CheckedBox" size="24px" />
-      ) : (
-        <SvgIcon name="EmptyBox" size="24px" />
-      )}
+      <TouchableOpacity onPress={() => _toggleTask(id)}>
+        {completed ? (
+          <SvgIcon name="CheckedBox" size="24px" />
+        ) : (
+          <SvgIcon name="EmptyBox" size="24px" />
+        )}
+      </TouchableOpacity>
     </Container>
   );
 }
