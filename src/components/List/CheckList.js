005e755e6ce8c652de61c4f0b0ca5d61c0bae764
flowersayo <flowersayo0811@ewhain.net>
2022-08-18 21:24:42 +0900

    feat: load & store & get tasks
    react-native-loading-spinner-overlay 를 활용한 로딩창 구현 및 local storage를 활용한 데이터 저장
    
    resolves #33

--- src/components/List/CheckList.js
+++ src/components/List/CheckList.js
@@ -1,5 +1,5 @@
 import {View, Text, Pressable} from 'react-native';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import font from '../../styles/font';
 import List from './List';
 import ListInput from './ListInput';
@@ -7,14 +7,20 @@ import styled from 'styled-components';
 import {theme} from '../../styles/theme';
 import {useCheckDeleteAllTasksModal} from '../../modal/recoil/useModals';
 import CheckDeleteAllTasksModal from '../../modal/modals/Around/CheckDeleteAllTasksModal';
+import {saveData, getData} from '../../services/LocalStorage';
+import Spinner from 'react-native-loading-spinner-overlay';
 
-export default function CheckList() {
-  const [mode, setMode] = useState('view'); // view :조회모드 , edit : 삭제모드
-  const [tasks, setTasks] = useState({
+/*
+{
     1: {id: 1, text: '잠옷바지', completed: false},
     2: {id: 2, text: '물티슈', completed: false},
     3: {id: 3, text: '양말', completed: false},
-  });
+  }
+  */
+export default function CheckList() {
+  const [mode, setMode] = useState('view'); // view :조회모드 , edit : 삭제모드
+  const [tasks, setTasks] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
   const {openModal} = useCheckDeleteAllTasksModal();
 
   const _addTask = text => {
@@ -24,27 +30,49 @@ export default function CheckList() {
       [ID]: {id: ID, text: text, completed: false},
     };
 
-    setTasks({...tasks, ...newTaskObject});
+    storeData({...tasks, ...newTaskObject});
+  };
+
+  const storeData = async tasks => {
+    await saveData('tasks', JSON.stringify(tasks));
+    setTasks(tasks);
+  };
+
+  const loadData = async () => {
+    const loadedData = await getData('tasks');
+
+    if (loadedData) setTasks(loadedData);
   };
 
   const _toggleTask = id => {
     const currentTask = Object.assign({}, tasks);
     currentTask[id]['completed'] = !currentTask[id]['completed'];
-    setTasks(currentTask);
+    storeData(currentTask);
   };
 
   const _deleteTask = id => {
     const currentTask = Object.assign({}, tasks);
     delete currentTask[id];
-    setTasks(currentTask);
+    storeData(currentTask);
   };
 
   const _deleteAllTask = () => {
-    setTasks({});
+    storeData({});
   };
+
+  useEffect(() => {
+    loadData();
+    setIsLoading(false);
+  }, []);
 
   return (
     <View>
+      <Spinner
+        cancelable={true}
+        color={theme.colors.main}
+        visible={isLoading}
+        textContent="Loading..."
+      />
       {mode == 'view' ? (
         <TextContainer1>
           <Pressable
@@ -87,7 +115,6 @@ export default function CheckList() {
     </View>
   );
 }
-
 const TextContainer1 = styled.Pressable`
   display: flex;
   align-items: flex-end;
