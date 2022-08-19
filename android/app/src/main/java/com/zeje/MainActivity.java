9fd84479f1b854aae9eeb1a8ea837fa4b934fc61
SJ-Lee33 <77518932+SJ-Lee33@users.noreply.github.com>
2022-08-19 18:42:19 +0900

    fix: splash 버그 해결

--- android/app/src/main/java/com/zeje/MainActivity.java
+++ android/app/src/main/java/com/zeje/MainActivity.java
@@ -3,7 +3,7 @@ import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
-import org.devio.rn.splashscreen.SplashScreen;
+import org.devio.rn.splashscreen.SplashScreen; 
 
 public class MainActivity extends ReactActivity {
 
