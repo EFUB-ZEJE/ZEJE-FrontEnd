9c8fd5fabb64d5b6a94e6756b83ad80f9222634f
SJ-Lee33 <77518932+SJ-Lee33@users.noreply.github.com>
2022-08-21 22:28:16 +0900

    feat: 카카오 로그인
    AsyncStorage 에 `key='token'` 으로 토근(id, accessToken, refreshToken)을 저장합니다.
    
    `.env` 파일에 카카오 네이티브 앱 키를 추가해 주셔야 합니다. 노션을 확인해 주세요!

--- src/screens/Login/LoginScreen.js
+++ src/screens/Login/LoginScreen.js
@@ -2,19 +2,72 @@ import React from 'react';
 import styled from 'styled-components/native';
 import {Center} from 'native-base';
 import {KakaoLoginButton, LogoLogin} from '../../assets';
+import {
+  getProfile as getKakaoProfile,
+  login,
+  logout,
+  unlink,
+} from '@react-native-seoul/kakao-login';
+import {saveData, getData} from '../../services/LocalStorage';
 
 export default function LoginScreen({navigation}) {
+  // 카카오 로그인
+  const signInWithKakao = async () => {
+    try {
+      const token = await login();
+      saveData('token', JSON.stringify(token));
+      navigation.navigate('TabNavigator');
+    } catch (err) {
+      console.error('login err', err);
+    }
+  };
+
+  //   // 카카오 로그아웃
+  //   const signOutWithKakao = async () => {
+  //     try {
+  //       const message = await logout();
+  //       setResult(message);
+  //     } catch (err) {
+  //       // eslint-disable-next-line no-console
+  //       console.error('signOut error', err);
+  //     }
+  //   };
+
+  //   // 프로필 불러오기
+  //   const getProfile = async () => {
+  //     try {
+  //       const profile = await getKakaoProfile();
+
+  //       setResult(JSON.stringify(profile));
+  //     } catch (err) {
+  //       // eslint-disable-next-line no-console
+  //       console.error('signOut error', err);
+  //     }
+  //   };
+
+  //   // 연동 해제
+  //   const unlinkKakao = async () => {
+  //     try {
+  //       const message = await unlink();
+
+  //       setResult(message);
+  //     } catch (err) {
+  //       // eslint-disable-next-line no-console
+  //       console.error('signOut error', err);
+  //     }
+  //   };
+
   return (
     <Center flex={1}>
       <LogoLogin />
-      <ButtonWrapper>
+      <ButtonWrapper onPress={() => signInWithKakao()}>
         <KakaoLoginButton />
       </ButtonWrapper>
     </Center>
   );
 }
 
-const ButtonWrapper = styled.View`
+const ButtonWrapper = styled.Pressable`
   position: absolute;
   bottom: 50px;
 `;
