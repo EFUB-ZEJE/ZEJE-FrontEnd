879db6c13eedde06f8662bbcd120a4667b6d1235
flowersayo <flowersayo0811@ewhain.net>
2022-08-23 00:40:46 +0900

    feat: 닉네임변경
    resolves #35

--- src/screens/Home/ProfileEditScreen.js
+++ src/screens/Home/ProfileEditScreen.js
@@ -7,13 +7,20 @@ import Profile from '../../components/home/MyPage/Profile';
 import {palette, theme} from '../../styles/theme';
 import {SizedBox} from '../../components';
 export default function ProfileEditScreen({navigation}) {
-  const [mode, setMode] = useState('view');
   const [text, setText] = useState('이펍');
 
   const _onSubmit = () => {
     // 닉네임 변경 요청
     // 닉네임 유효성 검사
     console.log('변경');
+  };
+
+  const _logout = () => {
+    // 로그아웃 요청
+  };
+
+  const _unregister = () => {
+    //회원탈퇴 요청
   };
 
   return (
@@ -30,17 +37,23 @@ export default function ProfileEditScreen({navigation}) {
         <Row>
           <View>
             <Caption>닉네임</Caption>
-            <Subhead3>{text}</Subhead3>
-            {/**  <TextInput
+
+            <TextInput
               value={text}
               placeholder="리스트를 입력해주세요."
               placeholderTextColor={palette.gray250}
               onChangeText={text => setText(text)}
-              style={{color: 'black'}}
+              style={{
+                color: 'black',
+                height: 22,
+                fontSize: 16,
+                fontWeight: 'bold',
+                padding: 0,
+              }}
               onSubmitEditing={_onSubmit}
               autoCorrect={false}
               maxLength={10}
-            />*/}
+            />
           </View>
           <TouchableOpacity onPress={() => console.log('닉네임을 변경합니다')}>
             <Subhead2 color={theme.colors.main}>변경</Subhead2>
@@ -51,9 +64,13 @@ export default function ProfileEditScreen({navigation}) {
           <Subhead3>카카오 계정에서 변경하실 수 있습니다.</Subhead3>
         </Col>
         <Right>
-          <Subhead2 color={theme.colors.main}>로그아웃 </Subhead2>
+          <TouchableOpacity onPress={_logout}>
+            <Subhead2 color={theme.colors.main}>로그아웃 </Subhead2>
+          </TouchableOpacity>
           <Subhead2 color={palette.gray200}> | </Subhead2>
-          <Subhead2 color={palette.gray300}>회원탈퇴 </Subhead2>
+          <TouchableOpacity onPress={_unregister}>
+            <Subhead2 color={palette.gray300}>회원탈퇴 </Subhead2>
+          </TouchableOpacity>
         </Right>
       </ScreenContainer>
     </>
