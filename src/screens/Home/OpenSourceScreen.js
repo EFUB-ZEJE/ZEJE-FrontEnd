402d7711285adf1426c168b38713f9df591744aa
flowersayo <flowersayo0811@ewhain.net>
2022-08-23 01:26:30 +0900

    feat: 마이페이지 - 정보 스크린
    이용약관,오픈소스 라이브러리 내용 수정 필요
    resolves #35

--- src/screens/Home/OpenSourceScreen.js
+++ src/screens/Home/OpenSourceScreen.js
@@ -1,10 +1,24 @@
+//이용약관
+
 import {View, Text} from 'react-native';
 import React from 'react';
+import {Subhead3, Body1} from '../../styles/font';
+import {ScreenHeader, ScreenContainer} from '../../components';
+import {palette} from '../../styles/theme';
 
-export default function OpenSourceScreen() {
+export default function TosScreen({navigation}) {
   return (
-    <View>
-      <Text>OpenSourceScreen</Text>
-    </View>
+    <>
+      <ScreenHeader
+        navigation={navigation}
+        screenTitle="오픈소스 라이브러리"
+        canGoBack={true}
+        canSearch={false}
+      />
+      <ScreenContainer>
+        <Subhead3>오픈소스 라이브러리명</Subhead3>
+        <Body1 color={palette.gray350}>설명</Body1>
+      </ScreenContainer>
+    </>
   );
 }
--- src/screens/Home/TosScreen.js
+++ src/screens/Home/TosScreen.js
@@ -2,11 +2,22 @@
 
 import {View, Text} from 'react-native';
 import React from 'react';
-
-export default function TosScreen() {
+import {Subhead3, Body1} from '../../styles/font';
+import {ScreenHeader, ScreenContainer} from '../../components';
+import {palette} from '../../styles/theme';
+export default function TosScreen({navigation}) {
   return (
-    <View>
-      <Text>TosScreen</Text>
-    </View>
+    <>
+      <ScreenHeader
+        navigation={navigation}
+        screenTitle="이용약관"
+        canGoBack={true}
+        canSearch={false}
+      />
+      <ScreenContainer>
+        <Subhead3>약관명</Subhead3>
+        <Body1 color={palette.gray350}>설명</Body1>
+      </ScreenContainer>
+    </>
   );
 }
