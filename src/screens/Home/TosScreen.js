06732d70ee644a157ec4d7591892750024f84e28
flowersayo <flowersayo0811@ewhain.net>
2022-08-30 02:16:02 +0900

    chore: 마이페이지 정리
    버전 정보 수정(1.0.0) , 이용약관 링크처리
    알람 및 내 리뷰 관리 페이지 임시처리

--- src/screens/Home/TosScreen.js
+++ src/screens/Home/TosScreen.js
@@ -1,6 +1,6 @@
 //이용약관
 
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity, Linking} from 'react-native';
 import React from 'react';
 import {Subhead3, Body1} from '../../styles/font';
 import {ScreenHeader, ScreenContainer} from '../../components';
@@ -15,8 +15,18 @@ export default function TosScreen({navigation}) {
         canSearch={false}
       />
       <ScreenContainer>
-        <Subhead3>약관명</Subhead3>
-        <Body1 color={palette.gray350}>설명</Body1>
+        <Subhead3>이용약관</Subhead3>
+
+        <TouchableOpacity
+          onPress={() =>
+            Linking.openURL(
+              'https://www.notion.so/a64e6dce87134d989a9403320e424387',
+            )
+          }>
+          <Body1 color={'blue'}>
+            https://www.notion.so/a64e6dce87134d989a9403320e424387
+          </Body1>
+        </TouchableOpacity>
       </ScreenContainer>
     </>
   );
