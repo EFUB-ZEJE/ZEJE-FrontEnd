177a48835b49ecffb3b3f79e0df735b0200a4471
flowersayo <flowersayo0811@ewhain.net>
2022-08-31 16:59:36 +0900

    feat : List Services 생성
    resolves #44

---
+++ src/services/ListService.js
@@ -0,0 +1,16 @@
+import API from '../utils/api';
+import {getData} from '../data/LocalStorage';
+import {ACCESS_TOKEN} from '../data/LocalStorage';
+
+const ListService = {
+  getWishList: async () =>
+    API.get(`/wish`, {
+      headers: {'X-AUTH-TOKEN': await getData(ACCESS_TOKEN)},
+    }),
+  addWishList: async spotId =>
+    API.get(`/wish/${spotId}`, {
+      headers: {'X-AUTH-TOKEN': await getData(ACCESS_TOKEN)},
+    }),
+};
+
+export default ListService;
