bff80882db2208c674be69350d64f8ac17a8b355
flowersayo <flowersayo0811@ewhain.net>
2022-08-31 16:59:36 +0900

    feat : List Services 생성
    resolves #44

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,8 @@
-import axios from 'axios';
 import {BASE_URL, EV_STATION_BASE_URL} from '../constants/config';
 import Config from 'react-native-config';
-
+import {getData} from '../data/LocalStorage';
+import axios, {AxiosInstance} from 'axios';
+import {ACCESS_TOKEN} from '../data/LocalStorage';
 // 로그인 - 토큰 발급 용 api
 export const GET_ACCESS_TOKEN_API = axios.create({
   baseURL: BASE_URL,
@@ -14,6 +15,15 @@ export const FRUIT_BOX_POINT_API = axios.create({
     'Content-Type': 'application/json',
   },
 });
+
+const API = axios.create({
+  baseURL: BASE_URL, // 기본 서버 주소 입력
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default API;
 
 // 전기차 충전소 조회용 Axios
 export const EV_STATION_API = axios.create({
