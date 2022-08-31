323f4cde1a5edd46874d967e19d3dfc4fae31582
flowersayo <flowersayo0811@ewhain.net>
2022-08-31 17:38:52 +0900

    feat: 위시리스트 조회
    resolves #44

--- src/components/List/WishList.js
+++ src/components/List/WishList.js
@@ -1,45 +1,26 @@
 import {View, Text} from 'react-native';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import font from '../../styles/font';
 import SortButton from '../Around/SortButton';
 import ImageCard from '../Around/ImageCard';
-
+import ListService from '../../services/ListService';
 export default function WishList() {
   const [sortId, setSortId] = useState(0); // 0: 최신순, 1: 과거순
   //true 상태 data 만 들어옴
 
-  const [wishList, setWishList] = useState([
-    {
-      wishId: 2,
-      userId: 5,
-      spotDTO: {
-        spotId: 2,
-        contentId: 0,
-        category: '여행',
-        type: '비건, 식당',
-        name: '아살람 레스토랑',
-        location: '제주특별자치도 제주시 삼도이동 1-2 1층',
-        description:
-          '아살람레스토랑은 예멘출신의 경험이 많은 쉐프가 한국인여성(와르다)과 결혼 후 한국 최초로 제주에 오픈한 아랍레스토랑이며, 모든 고기는 할랄을 사용합니다.\n무슬림이 기도할수 있는 기도실도 준비되어 있습니다.\n항상 사랑과 행복과 함께 요리하는 레스토랑이 되겠습니다.',
-        link: 'http://Instagram.com/jejuhalalasalam',
-      },
-    },
-    {
-      wishId: 3,
-      userId: 5,
-      spotDTO: {
-        spotId: 4,
-        contentId: 0,
-        category: '여행',
-        type: '비건, 식당',
-        name: '스토브온 제주',
-        location: '제주특별자치도 제주시 한림읍 상명리 1036',
-        description:
-          '천연 효모 발효 72시간 저온 숙성 도우와 직접 키운 채소 및 허브를 넣은 수제 소스로 만든 네모난 1인용 화덕피자입니다.',
-        link: 'http://pf.kakao.com/_ryIxexb',
-      },
-    },
-  ]);
+  const [wishList, setWishList] = useState([]);
+
+  useEffect(() => {
+    ListService.getWishList()
+      .then(res => {
+        if (res.status == 200) {
+          setWishList(res.data);
+        } else {
+          console.log('위시리스트를 가져오지 못했습니다.');
+        }
+      })
+      .catch(err => console.log(err));
+  }, []);
 
   const _handleLikeChange = id => {
     let newData = [...wishList];
@@ -82,6 +63,7 @@ export default function WishList() {
                 <ImageCard
                   key={place.wishId}
                   id={place.wishId}
+                  image={place.spotDTO.image}
                   title={place.spotDTO.name}
                   address={place.spotDTO.location}
                   liked={true}
