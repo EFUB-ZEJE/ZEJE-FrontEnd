824cc857acead419855980093f1610392eb12153
SJ-Lee33 <77518932+SJ-Lee33@users.noreply.github.com>
2022-09-03 11:26:26 +0900

    refector: 서비스에서 사용하는 axios instance 통일

--- src/services/FruitService.js
+++ src/services/FruitService.js
@@ -1,9 +1,9 @@
 import {ACCESS_TOKEN, getData} from '../data/LocalStorage';
-import {FRUIT_BOX_POINT_API} from '../utils/api';
+import API from '../utils/api';
 
 export const FruitService = {
   getFruitBoxPoint: async () =>
-    FRUIT_BOX_POINT_API.get('/users/account/profile/fruitBox', {
+    API.get('/users/account/profile/fruitBox', {
       headers: {'X-AUTH-TOKEN': await getData(ACCESS_TOKEN)},
     }),
 };
