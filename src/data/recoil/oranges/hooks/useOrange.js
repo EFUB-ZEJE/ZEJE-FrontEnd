8538aca584faaecdf570008d61de155fd8a41927
SJ-Lee33 <77518932+SJ-Lee33@users.noreply.github.com>
2022-09-03 15:57:45 +0900

    feat: 걸음수 충족 → 오렌지 획득 → 이후 처리
    오렌지 중 한라봉만

--- src/data/recoil/oranges/hooks/useOrange.js
+++ src/data/recoil/oranges/hooks/useOrange.js
@@ -4,8 +4,19 @@ import {orangesState} from '../states/orangesState';
 export const useOrange = () => {
   const [orange, setOrange] = useRecoilState(orangesState);
 
+  const deleteOrangeList = order => {
+    setOrange({
+      ...orange,
+      [order]: {
+        name: '',
+        maxWalk: 0,
+      },
+    });
+  };
+
   return {
     orange,
     setOrange,
+    deleteOrangeList,
   };
 };
