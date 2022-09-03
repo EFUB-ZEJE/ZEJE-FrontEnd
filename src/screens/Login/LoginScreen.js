eaa6255641facd2e8415c0bcc76972053bddba21
SJ-Lee33 <77518932+SJ-Lee33@users.noreply.github.com>
2022-09-03 21:42:21 +0900

    feat: 자동 로그인 (카카오)

--- src/screens/Login/LoginScreen.js
+++ src/screens/Login/LoginScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import styled from 'styled-components/native';
 import {Center} from 'native-base';
 import {KakaoLoginButton, LogoLogin} from '../../assets';
@@ -8,18 +8,24 @@ import {ACCESS_TOKEN, saveData} from '../../data/LocalStorage';
 
 export default function LoginScreen({navigation}) {
   const {kakaoLoginResponse, signInWithKakao, getProfile} = useKakaoLogin();
+
   const loginHandler = () => {
     signInWithKakao();
     getProfile();
   };
-  if (kakaoLoginResponse.id !== '') {
-    AuthService.getAccessToken(kakaoLoginResponse)
-      .then(res => {
-        saveData(ACCESS_TOKEN, res.data);
-        navigation.navigate('TabNavigator');
-      })
-      .catch(err => console.error(err));
-  }
+
+  useEffect(() => {
+    if (kakaoLoginResponse.id !== '') {
+      AuthService.getAccessToken(kakaoLoginResponse)
+        .then(res => {
+          saveData(ACCESS_TOKEN, res.data);
+          navigation.navigate('TabNavigator');
+        })
+        .catch(err => console.error(err));
+    } else {
+      getProfile();
+    }
+  }, [kakaoLoginResponse]);
 
   return (
     <Center flex={1}>
