d7d8637ffdd005817f1962a33294a5748c34002e
SJ-Lee33 <77518932+SJ-Lee33@users.noreply.github.com>
2022-09-03 16:06:47 +0900

    feat: 오렌지 획득처리 완성

--- src/modal/modals/Home/OrangeGreenModal.js
+++ src/modal/modals/Home/OrangeGreenModal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useOrangeGreenModal} from '../../recoil/useModals';
 import ModalSheet from '../../../components/common/modal/ModalSheet';
 import {Column} from 'native-base';
@@ -7,10 +7,32 @@ import {Body_long1, Subhead_long3} from '../../../styles/font';
 import ModalButton from '../../../components/home/ModalButton';
 import OrangeModalProgressBar from '../../../components/home/oranges/OrangeModalProgressBar';
 import {ORANGES_LIST} from '../../../components/home/oranges/OrangeBox';
+import {usePedometer} from '../../../feature/pedometer/recoil/usePedometer';
+import {useFruitBoxPoint} from '../../../data/recoil/fruitBox/hooks/useFruitBoxPoint';
+import {useOrange} from '../../../data/recoil/oranges/hooks/useOrange';
+import {useFocusedOrangeOrder} from '../../../data/recoil/oranges/hooks/useFocusedOrangeOrder';
+import {saveOrangeList} from '../../../components/home/oranges/Orange';
 
 const OrangeGreenModal = () => {
   const {isModalOpen, closeModal} = useOrangeGreenModal();
   const maxWalk = ORANGES_LIST[2].maxWalk;
+  const {stepCount} = usePedometer();
+  const {addFruitBoxPoint} = useFruitBoxPoint();
+  const {orange, deleteOrangeList} = useOrange();
+  const {focusedOrangeOrder} = useFocusedOrangeOrder();
+
+  const orangeToPoint = () => {
+    addFruitBoxPoint({maxWalk: maxWalk});
+    closeModal();
+
+    // 리코일 오렌지리스트에서 삭제하기
+    deleteOrangeList(focusedOrangeOrder);
+  };
+
+  // 로컬 오렌지리스트에서 삭제하기
+  useEffect(() => {
+    saveOrangeList(orange);
+  }, [orange]);
 
   return (
     <ModalSheet isModalOpen={isModalOpen} closeModal={closeModal}>
@@ -22,7 +44,10 @@ const OrangeGreenModal = () => {
           됩니다. 시중에 보이는 덜 익은 귤인 하귤과는 다르다는 사실!
         </Body_long1>
         <OrangeModalProgressBar maxWalk={maxWalk} />
-        <ModalButton onPress={closeModal} text={'닫기'} />
+        {stepCount > maxWalk && (
+          <ModalButton onPress={() => orangeToPoint()} text={'획득하기'} />
+        )}
+        <ModalButton onPress={closeModal} text={'닫기'} white />
       </Column>
     </ModalSheet>
   );
--- src/modal/modals/Home/OrangeThousandModal.js
+++ src/modal/modals/Home/OrangeThousandModal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useOrangeThousandModal} from '../../recoil/useModals';
 import ModalSheet from '../../../components/common/modal/ModalSheet';
 import {Column} from 'native-base';
@@ -7,10 +7,32 @@ import {Body_long1, Subhead_long3} from '../../../styles/font';
 import ModalButton from '../../../components/home/ModalButton';
 import OrangeModalProgressBar from '../../../components/home/oranges/OrangeModalProgressBar';
 import {ORANGES_LIST} from '../../../components/home/oranges/OrangeBox';
+import {usePedometer} from '../../../feature/pedometer/recoil/usePedometer';
+import {useFruitBoxPoint} from '../../../data/recoil/fruitBox/hooks/useFruitBoxPoint';
+import {useOrange} from '../../../data/recoil/oranges/hooks/useOrange';
+import {useFocusedOrangeOrder} from '../../../data/recoil/oranges/hooks/useFocusedOrangeOrder';
+import {saveOrangeList} from '../../../components/home/oranges/Orange';
 
 const OrangeThousandModal = () => {
   const {isModalOpen, closeModal} = useOrangeThousandModal();
   const maxWalk = ORANGES_LIST[7].maxWalk;
+  const {stepCount} = usePedometer();
+  const {addFruitBoxPoint} = useFruitBoxPoint();
+  const {orange, deleteOrangeList} = useOrange();
+  const {focusedOrangeOrder} = useFocusedOrangeOrder();
+
+  const orangeToPoint = () => {
+    addFruitBoxPoint({maxWalk: maxWalk});
+    closeModal();
+
+    // 리코일 오렌지리스트에서 삭제하기
+    deleteOrangeList(focusedOrangeOrder);
+  };
+
+  // 로컬 오렌지리스트에서 삭제하기
+  useEffect(() => {
+    saveOrangeList(orange);
+  }, [orange]);
 
   return (
     <ModalSheet isModalOpen={isModalOpen} closeModal={closeModal}>
@@ -22,7 +44,10 @@ const OrangeThousandModal = () => {
           이름처럼 진한 향과 얇고 매끈한 껍질, 높은 당도가 특징입니다.
         </Body_long1>
         <OrangeModalProgressBar maxWalk={maxWalk} />
-        <ModalButton onPress={closeModal} text={'닫기'} />
+        {stepCount > maxWalk && (
+          <ModalButton onPress={() => orangeToPoint()} text={'획득하기'} />
+        )}
+        <ModalButton onPress={closeModal} text={'닫기'} white />
       </Column>
     </ModalSheet>
   );
