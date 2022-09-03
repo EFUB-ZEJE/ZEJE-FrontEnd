b1e008565fb5981e27c16a65eb78cac07d058fa7
SJ-Lee33 <77518932+SJ-Lee33@users.noreply.github.com>
2022-09-04 01:09:21 +0900

    feat: 자동로그인 시 로딩화면 추가

--- src/screens/Login/LoginScreen.js
+++ src/screens/Login/LoginScreen.js
@@ -1,20 +1,25 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from 'styled-components/native';
-import {Center, Pressable} from 'native-base';
+import {Center} from 'native-base';
 import {KakaoLoginButton, LogoLogin} from '../../assets';
 import {useKakaoLogin} from '../../data/recoil/kakaoLogin/hooks/useKakaoLogin';
 import AuthService from '../../services/AuthService';
 import {ACCESS_TOKEN, saveData} from '../../data/LocalStorage';
+import Spinner from 'react-native-loading-spinner-overlay';
+import {theme} from '../../styles/theme';
 
 export default function LoginScreen({navigation}) {
+  const [isLoading, setIsLoading] = useState(false);
   const {kakaoSignin, kakaoLoginResponse, signInWithKakao, getProfile} =
     useKakaoLogin();
 
   useEffect(() => {
     if (kakaoLoginResponse.id !== '' && kakaoSignin) {
+      setIsLoading(true);
       AuthService.getAccessToken(kakaoLoginResponse)
         .then(res => {
           saveData(ACCESS_TOKEN, res.data);
+          setIsLoading(false);
           navigation.navigate('TabNavigator');
         })
         .catch(err => console.error(err));
@@ -24,12 +29,20 @@ export default function LoginScreen({navigation}) {
   }, [kakaoLoginResponse.id, kakaoSignin]);
 
   return (
-    <Center flex={1}>
-      <LogoLogin />
-      <ButtonWrapper onPress={() => signInWithKakao()}>
-        <KakaoLoginButton />
-      </ButtonWrapper>
-    </Center>
+    <>
+      <Spinner
+        cancelable={true}
+        color={theme.colors.main}
+        visible={isLoading}
+        textContent="Loading..."
+      />
+      <Center flex={1}>
+        <LogoLogin />
+        <ButtonWrapper onPress={() => signInWithKakao()}>
+          <KakaoLoginButton />
+        </ButtonWrapper>
+      </Center>
+    </>
   );
 }
 
