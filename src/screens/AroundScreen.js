a1798302047938d2c90e95faa7b4545d7e88eef2
flowersayo <flowersayo0811@ewhain.net>
2022-09-04 22:48:08 +0900

    chore : 썸네일 처리
    썸네일 사진 등록, 썸네일 해상도 업그레이드, 친환경 스팟 제보 스크린 등록

--- src/screens/AroundScreen.js
+++ src/screens/AroundScreen.js
@@ -44,19 +44,22 @@ export default function AroundScreen({navigation}) {
           text="ZEJE의 추천 여행지"
           navigation={navigation}
           path="TourMain"
+          image="spot"
         />
         <SizedBox height={16} />
         <PhotoBanner
           text="ZEJE의 추천 액티비티"
           navigation={navigation}
           path="ActivityMain"
+          image="activity"
         />
+        <SizedBox height={32} />
         <CommonBanner
-          text="장소 제보하기"
+          text="친환경 스팟 제보하기"
           color={theme.colors.main}
           bgColor={palette.orange100}
           navigation={navigation}
-          path="ShareInfoMain"
+          onPress={() => navigation.navigate('ReportSpot')}
         />
       </ScreenContainer>
     </>
