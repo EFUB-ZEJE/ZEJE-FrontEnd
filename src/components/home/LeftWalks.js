d1f0082d82e68acc4e42ae9ab2017eb2e749452b
김서연 <86418674+flowersayo@users.noreply.github.com>
2022-09-13 03:40:22 +0900

    Revert "[#46] 걸음수, 오렌지 수 로컬에 저장하지 못하는 오류 수정"

--- src/components/home/LeftWalks.js
+++ src/components/home/LeftWalks.js
@@ -6,7 +6,7 @@ import {usePedometer} from '../../feature/pedometer/recoil/usePedometer';
 import {theme} from '../../styles/theme';
 
 function LeftWalks() {
-  const {stepCount, storeStepCount} = usePedometer();
+  const {stepCount, setStepCount} = usePedometer();
 
   return (
     <Row style={styles.container}>
@@ -17,7 +17,7 @@ function LeftWalks() {
       />
       <Pressable
         onPress={() => {
-          storeStepCount(stepCount + 1000);
+          setStepCount(stepCount + 1000);
         }}>
         <TextBold fontSize={12} color={theme.colors.font} fontWeight={700}>
           {stepCount.toString()}걸음
@@ -32,12 +32,12 @@ export default LeftWalks;
 const styles = StyleSheet.create({
   container: {
     height: 32,
-    width: '40%',
+    width: '45%',
     backgroundColor: 'white',
     borderRadius: 16,
     justifyContent: 'space-between',
     alignItems: 'center',
-    paddingHorizontal: 12,
+    paddingHorizontal: 10,
   },
 });
 
--- src/modal/modals/Around/ArriveSpotModal.js
+++ src/modal/modals/Around/ArriveSpotModal.js
@@ -10,8 +10,10 @@ import {CommonButton} from '../../../components';
 import {AroundService} from '../../../services/AroundService';
 import {useOrange} from '../../../data/recoil/oranges/hooks/useOrange';
 import {useFocusedOrangeOrder} from '../../../data/recoil/oranges/hooks/useFocusedOrangeOrder';
-import {ORANGES_LIST} from '../../../components/home/oranges/Orange';
-import {useLeftFlowers} from '../../../data/recoil/fruitBox/hooks/useLeftFlowers';
+import {
+  ORANGES_LIST,
+  saveOrangeList,
+} from '../../../components/home/oranges/Orange';
 
 const ModalContainer = styled.View`
   display: flex;
@@ -28,22 +30,19 @@ export default function ArriveSpotModal({
   setSproutPlace,
 }) {
   const [modalVisible, setModalVisible] = useRecoilState(ArriveSpotModalState);
-  const {orange, storeOrange} = useOrange();
+  const {orange, setOrange} = useOrange();
   const {focusedOrangeOrder} = useFocusedOrangeOrder();
-  const {addLeftFlowers} = useLeftFlowers();
 
   const addSprout = () => {
-    if (focusedOrangeOrder == 0) {
-      addLeftFlowers();
-    } else {
-      storeOrange({
-        ...orange,
-        [focusedOrangeOrder]: {
-          name: ORANGES_LIST[0].name,
-          maxWalk: ORANGES_LIST[0].maxWalk,
-        },
-      });
-    }
+    setOrange({
+      ...orange,
+      [focusedOrangeOrder]: {
+        name: ORANGES_LIST[0].name,
+        maxWalk: ORANGES_LIST[0].maxWalk,
+      },
+    });
+
+    saveOrangeList(orange);
   };
 
   const _handleBtnClick = async () => {
